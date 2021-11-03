2df0e5084aabaebb39c494f467337d9960d7c876
Aulia Rahman gnteng <83273573+Rhmn8@users.noreply.github.com>
2021-11-03 18:42:34 +0700

    Update group.js

--- message/group.js
+++ message/group.js
@@ -19,22 +19,22 @@ module.exports = async(xinz, anj, welcome, left) => {
     if (anj.action === 'add'){
         if (anj.participants[0] === xinz.user.jid){
             await sleep(5000)
-            xinz.updatePresence(anj.jid, Presence.composing)
-            xinz.sendMessage(anj.jid, `Hai aku ${botName}, silahkan kirim #menu`, MessageType.text)
+            aulia.updatePresence(anj.jid, Presence.composing)
+            aulia.sendMessage(anj.jid, `Hai aku ${botName}, silahkan kirim #menu`, MessageType.text)
         } else if (isWelcome){
             try {
-                var pic = await xinz.getProfilePicture(anj.participants[0])
+                var pic = await aulia.getProfilePicture(anj.participants[0])
             } catch {
                 var pic = 'https://i.ibb.co/Tq7d7TZ/age-hananta-495-photo.png'
             }
-            xinz.sendMessage(anj.jid, await getBuffer(pic), MessageType.image, {caption: `Hai @${anj.participants[0].split("@")[0]}, selamat datang di ${groupName}`, contextInfo: {"mentionedJid": [anj.participants[0]]}})
+            aulia.sendMessage(anj.jid, await getBuffer(pic), MessageType.image, {caption: `Hai @${anj.participants[0].split("@")[0]}, selamat datang di ${groupName}`, contextInfo: {"mentionedJid": [anj.participants[0]]}})
         }
     } else if (anj.action === 'remove' && isLeft){
         try {
-            var pic = await xinz.getProfilePicture(anj.participants[0])
+            var pic = await aulia.getProfilePicture(anj.participants[0])
         } catch {
             var pic = 'https://i.ibb.co/Tq7d7TZ/age-hananta-495-photo.png'
         }
-        xinz.sendMessage(anj.jid, await getBuffer(pic), MessageType.image, {caption: `Sayonara @${anj.participants[0].split("@")[0]}`, contextInfo: {"mentionedJid": [anj.participants[0]]}})
+        aulia.sendMessage(anj.jid, await getBuffer(pic), MessageType.image, {caption: `Sayonara @${anj.participants[0].split("@")[0]}`, contextInfo: {"mentionedJid": [anj.participants[0]]}})
     }
 }
