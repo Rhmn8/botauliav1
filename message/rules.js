22571ed3d56c0b2021067a6c0e854c45b5afcb92
Aulia Rahman gnteng <83273573+Rhmn8@users.noreply.github.com>
2021-11-18 16:56:44 +0700

    nambah aje
    nambah rules- hihihi

--- message/rules.js
+++ message/rules.js
@@ -1,12 +1,15 @@
 exports.rules = (botName) => {
-         return `「 RULES SKIMURA BOT 」
+         return `「 RULES ${botName} 」
          
 • JANGAN VC/TELPON BOT SKIMURA AUTO BLOCK
 • BOT INI AUTO RESPON KALAU ANDA KIRIM CMD
 • BOT SKIMURA TIDAK MENERIMA SEWA BOT GRATIS!
 • JANGAN SPAM PADA BOT INI! KALAU SPAM NANTI ANDA TERBLOCK
-• BOT INI MENGGUNAKAN PAKET/TERMUX/NGOPI DULU BANG
+• BOT INI MENGGUNAKAN PAKET/TERMUX/WiFi NGOPI DULU BANG
 • TIDAK MENERIMA GRATIS USER PREMIUM!
+• BOT INI TIDAK DOWNLOAD OTOMATIS!
+Mau beli premium? 10k permanen
+🪀 : https://wa.me/6285821676621
 
-                         「 SKIMURA BOT 」`
+-----------------「 ${botName} 」-----------------`
 }
